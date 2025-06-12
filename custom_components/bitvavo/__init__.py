8a1e4af15d7ab0fcb10a897e9211bf522cfe08ec
Mysticsilent <dirkvanlierop@gmail.com>
2025-06-12 08:22:39 +0200

    Update __init__.py
    Change: 
    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
    
    To:
    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
    
    As Homeassistant doesn't support the old method anymore

--- custom_components/bitvavo/__init__.py
+++ custom_components/bitvavo/__init__.py
@@ -17,14 +17,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # Forward de config entry naar de sensorplatform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Correcte manier in nieuwere versies van Home Assistant
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Verwijder de integratie netjes."""
-    if unload_ok := await hass.config_entries.async_forward_entry_unload(entry, "sensor"):
+    unload_ok = all(
+        await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    )
+    if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
